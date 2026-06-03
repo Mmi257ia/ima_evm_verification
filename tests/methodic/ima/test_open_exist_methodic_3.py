b0942b6e89213615b221de07d55c858470fea8ed
Анна Федорова <annafedorova@MacBook-Air-Anna-7.local>
2026-06-03 18:06:38 +0300

    test_open_exist for all files from methodic

---
+++ tests/methodic/ima/test_open_exist_methodic_3.py
@@ -0,0 +1,43 @@
+
+from os import O_RDONLY, O_WRONLY, O_RDWR
+from pytest import fixture
+from tests.spec import LinuxTestSpec
+import os
+
+
+@fixture(params=[
+    ("read", os.O_RDONLY),
+    ("write", os.O_WRONLY),
+    ("rdwr", os.O_RDWR),
+], ids=["read", "write", "rdwr"])
+def access_mode(request):
+    return request.param
+
+"""
+из методики пункт 3
+некорректные контролируемые подсистемой обеспечения целостности 
+(с сохраненным некорректным хэш-кодом), 
+но изменяемые (без атрибута immutable)
+"""
+def test_open_exists_methodic_3(t: LinuxTestSpec, access_mode):
+    mode_name, flags = access_mode
+
+    policy_uid = 2000
+
+    # user with ima
+    ima_user = 'ima_user'
+    t.make_user(ima_user, uid=policy_uid)
+
+    t.setup_ima_policy(str(policy_uid))
+
+    t.make_dir('/dir', ima_user, ima_user, 0o755)
+    t.make_file('/dir/file', owner=ima_user, group=ima_user, mode=0o666)
+
+    t.add_setup(f'echo "test content" > /dir/file')
+    t.add_setup(f'cat /dir/file > /dev/null')
+
+    t.add_setup(f'echo "add data" > /dir/file')
+
+    with t.make_program_and_run(ima_user, ima_user, umask=0) as child:
+        # all should fail
+        child.open('/dir/file', flags, 0, fatal=False)
